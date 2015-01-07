c894f46dcd41766f60fb40faf2ac28cf5e00ccc9
Oh-Hyun Kwon <kwonoh@me.com>
2015-01-07 02:39:55 -0800

    Conver to Tabs (UE4 Coding Standard)

--- OpenCL/Source/OpenCL/OpenCL.Build.cs
+++ OpenCL/Source/OpenCL/OpenCL.Build.cs
@@ -6,21 +6,21 @@ namespace UnrealBuildTool.Rules
 {
 	public class OpenCL : ModuleRules
 	{
-        private string ModulePath
-        {
-            get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
-        }
+		private string ModulePath
+		{
+			get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+		}
 
-        private string ThirdPartyPath
-        {
-            get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
-        }
+		private string ThirdPartyPath
+		{
+			get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+		}
 
 		public OpenCL(TargetInfo Target)
 		{
 			PublicIncludePaths.Add(
-                "OpenCL/Public"
-                );
+				"OpenCL/Public"
+				);
 
 			PublicDependencyModuleNames.AddRange(
 				new string[]
@@ -34,14 +34,14 @@ namespace UnrealBuildTool.Rules
 				}
 				);
 
-            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "Win64" : "Win32";
-            string OpenCLLibrariesPath = Path.Combine(ThirdPartyPath, "OpenCL", "Lib");
-            string NvidiaLibrariesPath = Path.Combine(OpenCLLibrariesPath, "NVIDIA", PlatformString);
-            string InelLibrariesPath = Path.Combine(OpenCLLibrariesPath, "Intel", PlatformString);
-            if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
+			string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "Win64" : "Win32";
+			string OpenCLLibrariesPath = Path.Combine(ThirdPartyPath, "OpenCL", "Lib");
+			string NvidiaLibrariesPath = Path.Combine(OpenCLLibrariesPath, "NVIDIA", PlatformString);
+			string InelLibrariesPath = Path.Combine(OpenCLLibrariesPath, "Intel", PlatformString);
+			if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
 			{
-                PublicAdditionalLibraries.Add(Path.Combine(NvidiaLibrariesPath, "OpenCL.lib"));
-                PublicAdditionalLibraries.Add(Path.Combine(InelLibrariesPath, "OpenCL.lib"));
+				PublicAdditionalLibraries.Add(Path.Combine(NvidiaLibrariesPath, "OpenCL.lib"));
+				PublicAdditionalLibraries.Add(Path.Combine(InelLibrariesPath, "OpenCL.lib"));
 			}
 			else if (Target.Platform == UnrealTargetPlatform.Mac)
 			{
--- OpenCL/Source/OpenCL/Public/IOpenCLPlugin.h
+++ OpenCL/Source/OpenCL/Public/IOpenCLPlugin.h
@@ -14,26 +14,26 @@ class IOpenCLPlugin : public IModuleInterface
 
 public:
 
-    /**
-     * Singleton-like access to this module's interface.  This is just for convenience!
-     * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
-     *
-     * @return Returns singleton instance, loading the module on demand if needed
-     */
-    static inline IOpenCLPlugin& Get()
-    {
-        return FModuleManager::LoadModuleChecked<IOpenCLPlugin>("OpenCLPlugin");
-    }
+	/**
+	 * Singleton-like access to this module's interface.  This is just for convenience!
+	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	 *
+	 * @return Returns singleton instance, loading the module on demand if needed
+	 */
+	static inline IOpenCLPlugin& Get()
+	{
+		return FModuleManager::LoadModuleChecked<IOpenCLPlugin>("OpenCLPlugin");
+	}
 
-    /**
-     * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
-     *
-     * @return True if the module is loaded and ready to use
-     */
-    static inline bool IsAvailable()
-    {
-        return FModuleManager::Get().IsModuleLoaded("OpenCLPLugin");
-    }
+	/**
+	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+	 *
+	 * @return True if the module is loaded and ready to use
+	 */
+	static inline bool IsAvailable()
+	{
+		return FModuleManager::Get().IsModuleLoaded("OpenCLPLugin");
+	}
 };
 
 DECLARE_LOG_CATEGORY_EXTERN(LogOpenCL, Log, All);
